4ee953681f92b3ea820819552d4267e0f9b89a53
mRcfps <1402491442@qq.com>
2017-05-28 10:34:46 +0800

    Update settings.py

--- csthome/settings.py
+++ csthome/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pkudv2dg+bshujs^zoo$(pi!%_sowc%b&m6$+pck0ad)jz!!v6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
