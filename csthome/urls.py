04c65c1c16b0339a30ad65c0cda66f01e6933b1a
mRcfps <1402491442@qq.com>
2017-05-29 16:02:02 +0800

    Update urls.py

--- csthome/urls.py
+++ csthome/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from home.views import welcome
+
 urlpatterns = [
     url(r'^home/', include('home.urls', namespace='home')),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^feedback/', include('feedback.urls', namespace='feedback')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', welcome),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
