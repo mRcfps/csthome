97e1ba3182b1442b4793738ac2c596bfa7073dfb
mRcfps <1402491442@qq.com>
2017-10-23 10:51:08 +0800

    Add fabric automation script.

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import *
+
+env.hosts = ['root@60.205.183.134']
+
+
+def deploy_with_private_key():
+    run("git pull")
+    run("docker-compose down")
+    run("docker-compose up --build -d")
+
+
+def deploy():
+    locals("fab deploy_with_private_key -i pf.pem")
