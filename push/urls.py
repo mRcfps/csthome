659ba267fb29d6c8878dbf5c7cb5f46a2968dc86
mRcfps <1402491442@qq.com>
2018-03-13 16:44:29 +0800

    Fix migration oversight in url

--- push/urls.py
+++ push/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path(
-        'push-notifications/<int:push_id>/$',
+        'push-notifications/<int:push_id>/',
         views.admin_send_push_notifications,
         name='push-notification'),
 ]
