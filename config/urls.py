f8e6759fefb789b27ee69edd6c5531613fee290c
mRcfps <1402491442@qq.com>
2018-03-20 21:20:24 +0800

    Clean up dependencies to ensure success build

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,6 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from rest_framework.documentation import include_docs_urls
-
 from home.views import welcome
 
 
@@ -15,6 +13,5 @@ urlpatterns = [
     path('push/', include('push.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
-    path('docs/', include_docs_urls(title='CSTHome API')),
     path('', welcome),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- csthome/push/views.py
+++ csthome/push/views.py
@@ -1,14 +1,8 @@
-import requests
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404, render
 from rest_framework import status
 
 from .models import PushNotification
-
-APP_ID     = '7U37shB86JaWjHwIWSvVV8wB-gzGzoHsz'
-APP_KEY    = 'n5MyRip3G3NsBvNxSCX13ep8'
-MASTER_KEY = 'O7MNG79CrA1I1hh0B5Rq4ONP'
-PUSH_URL   = 'https://api.leancloud.cn/1.1/push'
 
 
 @staff_member_required
@@ -18,12 +12,7 @@ def admin_send_push_notifications(request, push_id):
     push = get_object_or_404(PushNotification, id=push_id)
 
     # Send requests to LeanCloud REST API for push notifications
-    headers = {'X-LC-Id': APP_ID,
-               'X-LC-Key': APP_KEY,
-               'Content-Type': 'application/json'}
-    response = requests.post(PUSH_URL,
-                             json={'data': push.content},
-                             headers=headers)
+    response = None
 
     if response.status_code == status.HTTP_200_OK:
         push.has_pushed = True
