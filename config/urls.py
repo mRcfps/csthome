f66eb03c914f63811b335ebf8998ad9dccc5d5db
mRcfps <1402491442@qq.com>
2018-03-23 17:31:03 +0800

    Fix root URLConf bug
    * Fix invalid import of home views
    * Fix `include` mistaken urls params

--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,13 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from home.views import welcome
-
+from csthome.home.views import welcome
 
 urlpatterns = [
-    path('home/', include('home.urls', namespace='home')),
-    path('users/', include('users.urls', namespace='users')),
-    path('feedback/', include('feedback.urls', namespace='feedback')),
-    path('push/', include('push.urls', namespace='push')),
+    path('home/', include('csthome.home.urls', namespace='home')),
+    path('users/', include('csthome.users.urls', namespace='users')),
+    path('feedback/', include('csthome.feedback.urls', namespace='feedback')),
+    path('push/', include('csthome.push.urls', namespace='push')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('', welcome),
